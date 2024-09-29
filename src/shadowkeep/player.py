1b1003f0e379e8bd55c7a1c403ae1e6ee72ef077
David Kappel <kappeldav@gmail.com>
2024-09-29 13:56:42 +0200

    - fixed variable names - fixed coordinates

--- src/shadowkeep/player.py
+++ src/shadowkeep/player.py
@@ -1,7 +1,7 @@
 import pygame
 from shadowkeep.config import TILE_HEIGHT, TILE_WIDTH
 
-from src.shadowkeep.grid import coords_transform_pair, coords_transform_single
+from shadowkeep.grid import coords_transform_pair, coords_transform_single
 
 PLAYER_HEIGHT = TILE_HEIGHT
 PLAYER_WIDTH = TILE_WIDTH
@@ -9,8 +9,8 @@ PLAYER_WIDTH = TILE_WIDTH
 
 class Player:
     def __init__(self, window, map):
-        self.x = coords_transform_single(3)
-        self.y = coords_transform_single(3)
+        self.x = 3
+        self.y = 3
         self.window = window
         self.surface = pygame.surface.Surface((PLAYER_WIDTH, PLAYER_HEIGHT))
         self.surface.fill((255, 250, 250))
@@ -19,13 +19,13 @@ class Player:
     def move(self):
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_w]:
-            self.y -= TILE_HEIGHT
+            self.y -= 1
         if pressed_keys[pygame.K_s]:
-            self.y += TILE_HEIGHT
+            self.y += 1
         if pressed_keys[pygame.K_d]:
-            self.x += TILE_WIDTH
+            self.x += 1
         if pressed_keys[pygame.K_a]:
-            self.x -= TILE_WIDTH
+            self.x -= 1
 
     def blit(self):
-        self.window.blit(self.surface, (self.x, self.y))
+        self.window.blit(self.surface, coords_transform_pair(self.x, self.y))
