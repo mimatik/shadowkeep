1012c116d97fc02f7a48c7d7aa07d3ec9b97ce6f
kappemic <kappelmichal@gmail.com>
2024-10-15 18:36:36 +0200

    buble scaling

--- src/shadowkeep/text.py
+++ src/shadowkeep/text.py
@@ -6,11 +6,13 @@ class TextInput:
     def __init__(self, game, theme="light"):
         self.game = game
         self.text = ""
-        self.bg_surface = pygame.surface.Surface((250, 150))
+
+        self.bg_surface = pygame.surface.Surface((0, 0))
 
         self.font_size = 20
         self.line_height = round(self.font_size * 0.9)
         self.font = pygame.font.Font(None, self.font_size)
+        self.padding = 8
 
         self.themes = {
             "dark": {"background": (0, 0, 0), "text": (255, 255, 255)},
@@ -19,13 +21,31 @@ class TextInput:
         self.theme_color = self.themes[theme]
 
     def surface_set(self, bg_surface, text, position):
-        self.bg_surface.fill(self.theme_color["background"])
+        height = 0
+        width = 0
+        width = 0
+
+        wrapped_text_surface = pygame.surface.Surface((250, 500), pygame.SRCALPHA)
+        wrapped_text_surface.fill((0, 0, 0, 0))
         wrapped_text = textwrap.wrap(text, width=32)
 
-        for line in wrapped_text:
-            text_surface = self.font.render(line, True, self.theme_color["text"])
-            bg_surface.blit(text_surface, (position))
-            position = (position[0], position[1] + self.line_height)
+        if wrapped_text:
+            for line in wrapped_text:
+                text_surface = self.font.render(line, True, self.theme_color["text"])
+                wrapped_text_surface.blit(text_surface, (position))
+                position = (position[0], position[1] + self.line_height)
+
+                width = max(width, pygame.surface.Surface.get_width(text_surface))
+                height += self.line_height
+
+        bg_surface = pygame.surface.Surface(
+            (
+                width + (self.padding * 2),
+                height + (self.padding * 2),
+            )
+        )
+        bg_surface.fill(self.theme_color["background"])
+        bg_surface.blit(wrapped_text_surface, (0, 0))
 
         return bg_surface
 
