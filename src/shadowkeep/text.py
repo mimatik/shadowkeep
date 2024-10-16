19be4855c3c6bb2b76290dbdf93a4d8076d619e2
Martin Kappel <kilpak@gmail.com>
2024-10-16 06:13:36 +0200

    blink empty text bubble

--- src/shadowkeep/text.py
+++ src/shadowkeep/text.py
@@ -10,7 +10,7 @@ class TextInput:
         self.font_size = 20
         self.line_height = round(self.font_size * 0.9)
         self.font = pygame.font.Font(None, self.font_size)
-        self.padding = 8
+        self.padding = round(self.font_size * 0.5)
 
         self.themes = {
             "dark": {"background": (0, 0, 0), "text": (255, 255, 255)},
@@ -18,11 +18,12 @@ class TextInput:
         }
         self.theme_color = self.themes[theme]
 
-    def surface_set(self, text, position):
+    def surface_set(self, text):
         height = 0
         width = 0
+        position = (self.padding, self.padding)
 
-        wrapped_text_surface = pygame.surface.Surface((250, 500), pygame.SRCALPHA)
+        wrapped_text_surface = pygame.surface.Surface((300, 500), pygame.SRCALPHA)
         wrapped_text_surface.fill((0, 0, 0, 0))
         wrapped_text = textwrap.wrap(text, width=32)
 
@@ -35,17 +36,33 @@ class TextInput:
                 width = max(width, pygame.surface.Surface.get_width(text_surface))
                 height += self.line_height
 
-        bg_surface = pygame.surface.Surface(
-            (
-                width + (self.padding * 2),
-                height + (self.padding * 2),
+            self.surface_width = width + (self.padding * 2)
+            self.surface_height = height + (self.padding * 2) - 4
+
+            bg_surface = pygame.surface.Surface(
+                (
+                    self.surface_width,
+                    self.surface_height,
+                )
             )
-        )
-        bg_surface.fill(self.theme_color["background"])
-        bg_surface.blit(wrapped_text_surface, (0, 0))
+            bg_surface.fill(self.theme_color["background"])
+            bg_surface.blit(wrapped_text_surface, (0, 0))
+
+        else:
+            bg_surface = self.blink()
 
         return bg_surface
 
+    def blink(self):
+        size = (self.padding * 2, self.line_height + self.padding * 2 - 4)
+        blink_surface = pygame.Surface(size, pygame.SRCALPHA)
+        blink_surface.fill(self.theme_color["background"])
+
+        alpha = int(abs(pygame.time.get_ticks() / 500 % 2 - 1) * 255)
+        blink_surface.set_alpha(alpha)
+
+        return blink_surface
+
     def blit(self, text, bubble_position):
-        surface = self.surface_set(text, (10, 10))
+        surface = self.surface_set(text)
         self.game.ui_layer.place_surface(surface, (bubble_position))
