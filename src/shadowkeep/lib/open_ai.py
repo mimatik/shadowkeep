626f217490b8f9e87f80e527d953a43a2c3d9a6d
Michal Kappel <kappelmichal@gmail.com>
2024-10-24 05:32:47 +0200

    conversation history

--- src/shadowkeep/lib/open_ai.py
+++ src/shadowkeep/lib/open_ai.py
@@ -3,22 +3,41 @@ from shadowkeep.config import OPENAI_API_KEY
 
 
 client = OpenAI(
-
     api_key=OPENAI_API_KEY,
 )
+
 
 class ChatGTP:
     def __init__(self, game):
         self.game = game
         self.text = ""
+        self.conversation = []
+
     def open_ai_get_response(self):
+        self.conversation.append(
+            {
+                "role": "user",
+                "content": self.text,
+            }
+        )
+
         chat_completion = client.chat.completions.create(
             messages=[
                 {
-                    "role": "user",
-                    "content": self.text,
-                }
+                    "role": "system",
+                    "content": "Jses prisera, vznikla zkrizenim monstra a ucitele cestiny, ktery rad vtipkuje. Mluv v hadankach a vyhodnocuj odpovedi. Komunikuj v cestine.",
+                },
+                *self.conversation,
             ],
             model="gpt-4o-mini",
         )
-        return chat_completion.choices[0].message.content
+
+        assistant_message = chat_completion.choices[0].message.content
+        self.conversation.append(
+            {
+                "role": "assistant",
+                "content": assistant_message,
+            }
+        )
+
+        return assistant_message
