c862c9073e5065844ef5ecf5389c8ec9a498822c
Jakub Dolejsek <jakub.dolejsek@gmail.com>
2024-11-14 13:34:29 +0100

    fix door image name, 0 keys by default

--- src/shadowkeep/game.py
+++ src/shadowkeep/game.py
@@ -3,24 +3,23 @@ import random
 import pygame
 from PIL import Image
 
-
 from shadowkeep import config
-from shadowkeep.config import IMG_DIR, TILE_HEIGHT, TILE_WIDTH, AUDIO_DIR
+from shadowkeep.config import AUDIO_DIR, IMG_DIR, TILE_HEIGHT, TILE_WIDTH
+from shadowkeep.dialog import Dialog
 from shadowkeep.layer import Layer
 from shadowkeep.lib.coordinates import Coordinates
+from shadowkeep.lib.open_ai import ChatGTP
 from shadowkeep.map import Map
 from shadowkeep.monster import (
     BadMonster,
-    Fireball,
-    FireballLauncher,
-    TalkingMonster,
-    Key,
     Door,
     End,
+    Fireball,
+    FireballLauncher,
+    Key,
+    TalkingMonster,
 )
 from shadowkeep.player import Player
-from shadowkeep.dialog import Dialog
-from shadowkeep.lib.open_ai import ChatGTP
 
 
 class Game:
@@ -48,7 +47,7 @@ class Game:
         self.firebals = []
 
         self.current_turn = 0
-        self.keys = 1
+        self.keys = 0
 
         self.dialog = Dialog(self)
 
